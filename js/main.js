2d11600496729bf8e46f49d3abd8b2e4a62adbbb
Gmaboy <fontillasgreencemark@gmail.com>
2025-02-09 00:22:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY 18TH BITHDAY FION LOVE YOU <3').split('')
+    const titles = ('HAPPY 18TH BIRTHDAY FION LOVE YOU <3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
